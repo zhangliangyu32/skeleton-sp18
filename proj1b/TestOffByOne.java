765afd490600d38a2c1b942cd92ce49a0baa7723
张良宇 <zhangliangyu@zhangliangyudeMacBook-Pro.local>
2022-04-24 21:58:09 +0800

    debug proj1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -10,8 +10,12 @@ public class TestOffByOne {
     @Test
     public void testEqualChars(){
         assertTrue(offByOne.equalChars('a', 'b'));
-        assertTrue(offByOne.equalChars('d', 'c'));
-        assertFalse(offByOne.equalChars('b', 'b'));
-        assertFalse(offByOne.equalChars('b', 'r'));
+        assertTrue(offByOne.equalChars('r', 'q'));
+        assertTrue(offByOne.equalChars('&', '%'));
+
+        assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('a', 'z'));
+        assertFalse(offByOne.equalChars('a', 'e'));
+        assertFalse(offByOne.equalChars('a', 'A'));
     }
 }
